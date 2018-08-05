925dfe6197033acb2a714e729eb2c9a1e959e7c5
Jakub <kaminskiqba@gmail.com>
2018-08-05 17:17:02 +0200

    fix for ignore

--- lib/category_list.dart
+++ lib/category_list.dart
@@ -8,11 +8,15 @@ class CategoryList extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
     return new Scaffold(
-    body: new Column(
+    body: new Container(
+      child: Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         new RaisedButton(
           child: Text('CATEGORY'),
+          color: Colors.white,
+          elevation: 4.0,
+          splashColor: Colors.blueGrey,
           onPressed: () {
             Navigator.push(
               context,
@@ -21,6 +25,7 @@ class CategoryList extends StatelessWidget {
           },
         ),
       ],
+    ),
     ),
   );
      
