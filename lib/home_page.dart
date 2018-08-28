b609e337d0bd4cd424ec23b057d8f24b0a4f1d02
Jakub <kaminskiqba@gmail.com>
2018-08-28 21:26:33 +0200

    Update ver from 0.4.0 to 0.4.1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -98,7 +98,7 @@ class HomePageList extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                Text('Wersja: 0.4.0'),
+                Text('Wersja: 0.4.1'),
               ],
             ))
           ],
